eaf8818004234c0902182a6ef34f13338dd2c32c
ZNHuang <39626298+ZNHuang@users.noreply.github.com>
2024-04-26 23:17:45 -0400

    display CPU usage in number and bar

--- sketch_lcd_display/sketch_lcd_display.ino
+++ sketch_lcd_display/sketch_lcd_display.ino
@@ -2,6 +2,12 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 String Delimiter = ",";
+byte zero[8] = {0b00000,0b00000,0b00000,0b00000,0b00000,0b00000,0b00000,0b10000};
+byte one[8] = {0b10000,0b10000,0b10000,0b10000,0b10000,0b10000,0b10000,0b10000};
+byte two[8] = {0b11000,0b11000,0b11000,0b11000,0b11000,0b11000,0b11000,0b11000};
+byte three[8] = {0b11100,0b11100,0b11100,0b11100,0b11100,0b11100,0b11100,0b11100};
+byte four[8] = {0b11110,0b11110,0b11110,0b11110,0b11110,0b11110,0b11110,0b11110};
+byte five[8] = {0b11111,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111};
 
 void setup() {
   if (! i2CAddrTest(0x27)) {
@@ -11,21 +17,46 @@ void setup() {
   Serial.begin(9600);
   lcd.init();
   lcd.backlight();
+  lcd.createChar(0, zero);
+  lcd.createChar(1, one);
+  lcd.createChar(2, two);
+  lcd.createChar(3, three);
+  lcd.createChar(4, four);
+  lcd.createChar(5, five);
 }
 
 void loop() {
+  lcd.setCursor(15, 0);
+  lcd.write(1);
   if (Serial.available()) {
     String command = Serial.readStringUntil('\n');
     command.trim();
     int index = command.indexOf(Delimiter);
-    float cpu = command.substring(0, index).toFloat();
-    String memory = command.substring(index + 1, command.length());
+    String cpu = command.substring(0, index);
+    float memory = command.substring(index + 1, command.length()).toFloat();
     lcd.setCursor(0, 0);
     lcd.print("CPU%:");
     lcd.print(cpu);
-    lcd.setCursor(0, 1);
-    lcd.print("Mem%:");
-    lcd.print(memory);
+    //lcd.setCursor(0, 0);
+    //lcd.print("Mem%:");
+    //lcd.print(memory);
+    displayBar(1, cpu.toFloat());
+  }
+}
+
+void displayBar(int row, float value) {
+  int bar_count = 80 * (value / 100); //80 = 16 * 5
+  for (int i = 0; i < 16; i++) {
+    lcd.setCursor(i, row);
+    if (bar_count > 5) {
+      lcd.write(5);
+      bar_count -= 5;
+    } else if (bar_count > 0) {
+      lcd.write(bar_count);
+      bar_count = 0;
+    } else {
+      lcd.write(0);
+    }
   }
 }
 
